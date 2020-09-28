add036905f687a24480712268be070c5ede9767f
Kasutan <contact@kasutan.pro>
2020-09-28 17:57:16 +0200

    Sections services accueil : colonne cliquable Accepter renvois à la ligne titres

--- template-parts/page/home-services.php
+++ template-parts/page/home-services.php
@@ -21,16 +21,20 @@
 					<?php if( get_theme_mod( 'services_icon_' . $i ) ) { ?>
 						<div class="services-icon"><span class="ei <?php echo esc_attr( get_theme_mod( 'services_icon_' . $i ) ); ?>"></span></div>
 					<?php } ?>
+					<?php if( get_theme_mod( 'services_link_' . $i ) ) { ?><a href="<?php echo esc_url( get_theme_mod( 'services_link_' . $i ) ); ?>"><?php } ?>
 					<?php if( get_theme_mod( 'services_title_' . $i ) ) { ?>
-						<h4 class="services-title">
-							<?php if( get_theme_mod( 'services_link_' . $i ) ) { ?><a href="<?php echo esc_url( get_theme_mod( 'services_link_' . $i ) ); ?>"><?php } ?>
-								<?php echo esc_html( get_theme_mod( 'services_title_' . $i ) ); ?>
-							<?php if( get_theme_mod( 'services_link_' . $i ) ) { ?></a><?php } ?>
-						</h4>
+						<?php $titre=esc_html(get_theme_mod( 'services_title_' . $i ));
+						$titre=explode('|',$titre);
+						$titre=implode('<br>',$titre);
+						?>
+						<h3 class="services-title">
+								<?php echo $titre; ?>
+						</h3>
 					<?php } ?>
 					<?php if( get_theme_mod( 'services_text_' . $i ) ) { ?>
 						<div class="desc"><?php echo wp_kses_post( wpautop(get_theme_mod( 'services_text_' . $i )) ); ?></div>
 					<?php } ?>
+					<?php if( get_theme_mod( 'services_link_' . $i ) ) { ?></a><?php } ?>
 				</div>
 			</div>
 			<?php endfor; ?>
